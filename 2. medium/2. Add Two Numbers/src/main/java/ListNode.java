e0c0b3677122373167f6981db6c4e4d12f4c26e7
Finn Milner <finn.milner@outlook.com>
2025-01-26 15:03:56 +0000

    test: implement tests for problem 2

---
+++ 2. medium/2. Add Two Numbers/src/main/java/ListNode.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode() {}
+	ListNode(int val) { this.val = val; }
+	ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+
+	public int[] toArray() {
+		ArrayList<Integer> list = new ArrayList<>();
+		ListNode current = this;
+		while (current != null) {
+			list.add(current.val);
+			current = current.next;
+		}
+
+		return list.stream().mapToInt(i -> i).toArray();
+	}
+}
--- 2. medium/2. Add Two Numbers/src/test/java/SolutionTest.java
+++ 2. medium/2. Add Two Numbers/src/test/java/SolutionTest.java
@@ -3,4 +3,61 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class SolutionTest {
 	
+	@Test
+	void Example1() {
+		var solution = new Solution();
+		ListNode l1 = 
+			new ListNode(2,
+			new ListNode(4,
+			new ListNode(3
+		)));
+		ListNode l2 = 
+			new ListNode(5,
+			new ListNode(6,
+			new ListNode(4
+		)));
+		assertArrayEquals(
+			new int[]{7, 0, 8},
+			solution.addTwoNumbers(l1, l2).toArray()
+		);
+	}
+
+	@Test
+	void Example2() {
+		var solution = new Solution();
+		ListNode l1 = 
+			new ListNode(0
+		);
+		ListNode l2 = 
+			new ListNode(0
+		);
+		assertArrayEquals(
+			new int[]{0},
+			solution.addTwoNumbers(l1, l2).toArray()
+		);
+	}
+
+	@Test
+	void Example3() {
+		var solution = new Solution();
+		ListNode l1 = 
+			new ListNode(9,
+			new ListNode(9,
+			new ListNode(9,
+			new ListNode(9,
+			new ListNode(9,
+			new ListNode(9,
+			new ListNode(9
+		)))))));
+		ListNode l2 = 
+			new ListNode(9,
+			new ListNode(9,
+			new ListNode(9,
+			new ListNode(9
+		))));
+		assertArrayEquals(
+			new int[]{8, 9, 9, 9, 0, 0, 0, 1},
+			solution.addTwoNumbers(l1, l2).toArray()
+		);
+	}
 }
