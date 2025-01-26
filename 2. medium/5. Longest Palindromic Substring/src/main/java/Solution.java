f63330f91feea00bfafd5b62b5c25f23ae6aaf46
Finn Milner <finn.milner@outlook.com>
2025-01-26 20:20:49 +0000

    perf: improve accepted solution further!

--- 2. medium/5. Longest Palindromic Substring/src/main/java/Solution.java
+++ 2. medium/5. Longest Palindromic Substring/src/main/java/Solution.java
@@ -1,18 +1,21 @@
 class Solution {
 	public String longestPalindrome(String s) {
 		// Get length of string
-		int len = s.length();
+		int sLen = s.length();
 
 		// Return empty string if provided empty string
 		// Return single char if provided single char
-		if (len <= 1) return s;
+		if (sLen <= 1) return s;
 
 
 		// Initialise the max pointer
-		int max = len -1;
+		int maxP = sLen -1;
 
-		// Initialise the longest palindrome
-		String longest = null;
+		// Initialise the longest palindrome pointers
+		int longestLP = 0;
+		int longestRP = 0;
+
+		// Initialise the longest palindrome length
 		int longestLen = 1;
 
 
@@ -22,17 +25,18 @@ class Solution {
 			Start at 1 and end at max because we already know the longest 
 			palindrome is at least 1 char.
 		*/
-		for (int mid = 1; (mid <= max); mid++) {
+		for (int midP = 1; (midP <= maxP); midP++) {
 			// End when it is not possible to find a longer palindrome
-			if ((len - mid) < (longestLen / 2.0f)) break;
+			if ((sLen - midP) < (longestLen / 2.0f)) break;
 
-			// even
-			int l = mid -1;
-			int r = mid;
-			while (l >= 0 && r <= max && s.charAt(l) == s.charAt(r)) {
+			// Find palindrome with an even centre
+			int l = midP -1;
+			int r = midP;
+			while (l >= 0 && r <= maxP && s.charAt(l) == s.charAt(r)) {
 				int subLen = r - l + 1;
 				if (subLen > longestLen) {
-					longest = s.substring(l, r +1);
+					longestLP = l;
+					longestRP = r;
 					longestLen = subLen;
 				}
 				l--;
@@ -41,15 +45,16 @@ class Solution {
 
 
 			// End when it is not possible to find a longer palindrome
-			if ((len - mid) < (longestLen / 2.0f)) break;
+			if ((sLen - midP) < (longestLen / 2.0f)) break;
 
-			// odd
-			l = mid -1;
-			r = mid +1;
-			while (l >= 0 && r <= max && s.charAt(l) == s.charAt(r)) {
+			// Find palindrome with an odd centre
+			l = midP -1;
+			r = midP +1;
+			while (l >= 0 && r <= maxP && s.charAt(l) == s.charAt(r)) {
 				int subLen = r - l + 1;
 				if (subLen > longestLen) {
-					longest = s.substring(l, r +1);
+					longestLP = l;
+					longestRP = r;
 					longestLen = subLen;
 				}
 				l--;
@@ -57,9 +62,7 @@ class Solution {
 			}
 		}
 
-		// If no palindrome longer than 1 found, set longest to first char
-		if (longest == null) longest = s.substring(0, 1);
-
-		return longest;
+		// Return substring, will default to first char if no longer palindrome found
+		return s.substring(longestLP, longestRP +1);
 	}
 }
