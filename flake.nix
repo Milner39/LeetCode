74db4b3f6981601fca1afb9ed86a308a7eb17683
Milner39 <91906877+Milner39@users.noreply.github.com>
2025-09-19 22:07:39 +0100

    feat: add nix flake

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... } @ inputs: (
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+
+          packages = with pkgs; [
+            gradle
+          ];
+
+        };
+      }
+    )
+  );
+}
