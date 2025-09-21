8eee70f415dc30abe574a42379d510ff29f91363
Milner39 <91906877+Milner39@users.noreply.github.com>
2025-09-21 17:56:58 +0100

    refactor: I give up

--- solutions/3_Hard/4_Median-Of-Two-Sorted-Arrays/src/Solution.java
+++ solutions/3_Hard/4_Median-Of-Two-Sorted-Arrays/src/Solution.java
@@ -1,72 +1,75 @@
-class Solution {
-  public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+// class Solution {
+//   public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+//     var A = nums1;
+//     var B = nums2;
 
-    /* Make nums1 the smallest array
-      Picking the smallest array to do binary search on makes our time 
-      complexity `O(log(min(n, m)))`.
-    */
-    if (nums1.length > nums2.length) {
-      var tmp = nums1;
-      nums1 = nums2;
-      nums2 = tmp;
-    }
-
-
-    // Calculate helpful variables
-    int     total = nums1.length + nums2.length;
-    int     half  = total / 2;
-    boolean isOdd = (total % 2 == 1);
+//     /* Make A the smallest array
+//       Picking the smallest array to do binary search on makes our time 
+//       complexity `O(log(min(n, m)))`.
+//     */
+//     if (A.length > B.length) {
+//       var tmp = A;
+//       A = B;
+//       B = tmp;
+//     }
 
 
-    // Set pointers to either end of nums1 for binary search
-    int left = 0;
-    int right = nums1.length -1;
-
-    // Binary search
-    while (true) {
-      // Get middle pointer of nums1
-      int m1 = (left + right) / 2;
-
-      /* Get middle pointer of nums2
-        To calculate this position, we know that half of the numbers should be 
-        in the "low" group and half should be in the "high" group.
-
-        All the numbers in nums1 from 0 to m1 will be in our low group, so we 
-        can calculate how many numbers are left to be included in the low group.
-
-        This will be shown by this pointer for nums2.
-      */ 
-      int m2 = half - m1 -2;
+//     // Calculate helpful variables
+//     int     total = nums1.length + nums2.length;
+//     int     half  = total / 2;
+//     boolean isOdd = (total % 2 == 1);
 
 
-      // Get the values of each number at the border of the "low-high" groups
-      int low1  = (m1 >= 0)             ? nums1[m1]   : Integer.MIN_VALUE;
-      int high1 = (m1+1 < nums1.length) ? nums1[m1+1] : Integer.MAX_VALUE;
-      int low2  = (m2 >= 0)             ? nums2[m2]   : Integer.MIN_VALUE;
-      int high2 = (m2+1 < nums2.length) ? nums2[m2+1] : Integer.MAX_VALUE;
+//     // Set pointers to either end of A for binary search
+//     int l = 0;
+//     int r = A.length - 1;
+
+//     // Binary search
+//     while (true) {
+//       // Get middle pointer of A
+//       int i = (l + r) / 2;
+
+//       /* Get middle pointer of B
+//         To calculate this position, we know that half of the numbers should be 
+//         in the "low" group and half should be in the "high" group.
+
+//         All the numbers in A from 0 to i will be in our low group, so we 
+//         can calculate how many numbers from B are left to be included in the 
+//         low group.
+
+//         This will be shown by this pointer for B.
+//       */ 
+//       int j = half - i - 2;
 
 
-      // If our groups are correct
-      if (low1 <= high2 && low2 <= high1) {
-        // Odd
-        if (isOdd) {
-          return Math.min(high1, high2);
-        }
-
-        // Even
-        return (double) (Math.max(low1, low2) + Math.min(high1, high2)) / 2;
-      }
+//       // Get the values of each number at the border of the "low-high" groups
+//       int ALow  = (i >= 0)           ? A[i]     : Integer.MIN_VALUE;
+//       int AHigh = (i + 1 < A.length) ? A[i + 1] : Integer.MAX_VALUE;
+//       int BLow  = (j >= 0)           ? B[j]     : Integer.MIN_VALUE;
+//       int BHigh = (j + 1 < B.length) ? B[j + 1] : Integer.MAX_VALUE;
 
 
-      // nums1 low group too big
-      else if (low1 > high2) {
-        right = m1 -1;
-      }
+//       // If our groups are correct
+//       if (ALow <= BHigh && BLow <= AHigh) {
+//         // Odd
+//         if (isOdd) {
+//           return Math.min(AHigh, BHigh);
+//         }
 
-      // nums1 low group too small
-      else {
-        left = m1 +1;
-      }
-    }
-  }
-}
+//         // Even
+//         return (Math.max(ALow, BLow) + Math.min(AHigh, BHigh)) / 2.0;
+//       }
+
+
+//       // A low group too big
+//       else if (ALow > BHigh) {
+//         r = i - 1;
+//       }
+
+//       // A low group too small
+//       else {
+//         l = i + 1;
+//       }
+//     }
+//   }
+// }
--- solutions/3_Hard/4_Median-Of-Two-Sorted-Arrays/test/SolutionTest.java
+++ solutions/3_Hard/4_Median-Of-Two-Sorted-Arrays/test/SolutionTest.java
@@ -1,65 +1,65 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+// import org.junit.jupiter.api.Test;
+// import static org.junit.jupiter.api.Assertions.*;
 
-public class SolutionTest {
+// public class SolutionTest {
   
-  @Test
-  void Example1() {
-    var solution = new Solution();
-    var n1 = new int[]{1, 3};
-    var n2 = new int[]{2};
+//   @Test
+//   void Example1() {
+//     var solution = new Solution();
+//     var n1 = new int[]{1, 3};
+//     var n2 = new int[]{2};
 
-    assertEquals(
-      2.0,
-      solution.findMedianSortedArrays(n1, n2)
-    );
-  }
+//     assertEquals(
+//       2.0,
+//       solution.findMedianSortedArrays(n1, n2)
+//     );
+//   }
 
-  @Test
-  void Example2() {
-    var solution = new Solution();
-    var n1 = new int[]{1, 2};
-    var n2 = new int[]{3, 4};
+//   @Test
+//   void Example2() {
+//     var solution = new Solution();
+//     var n1 = new int[]{1, 2};
+//     var n2 = new int[]{3, 4};
 
-    assertEquals(
-      2.5,
-      solution.findMedianSortedArrays(n1, n2)
-    );
-  }
+//     assertEquals(
+//       2.5,
+//       solution.findMedianSortedArrays(n1, n2)
+//     );
+//   }
 
-  @Test
-  void Example3() {
-    var solution = new Solution();
-    var n1 = new int[]{1, 2, 3};
-    var n2 = new int[]{1, 3, 4};
+//   @Test
+//   void Example3() {
+//     var solution = new Solution();
+//     var n1 = new int[]{1, 2, 3};
+//     var n2 = new int[]{1, 3, 4};
 
-    assertEquals(
-      2.5,
-      solution.findMedianSortedArrays(n1, n2)
-    );
-  }
+//     assertEquals(
+//       2.5,
+//       solution.findMedianSortedArrays(n1, n2)
+//     );
+//   }
 
-  @Test
-  void Example4() {
-    var solution = new Solution();
-    var n1 = new int[]{1, 2, 3, 4, 5};
-    var n2 = new int[]{6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
+//   @Test
+//   void Example4() {
+//     var solution = new Solution();
+//     var n1 = new int[]{1, 2, 3, 4, 5};
+//     var n2 = new int[]{6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17};
 
-    assertEquals(
-      9,
-      solution.findMedianSortedArrays(n1, n2)
-    );
-  }
+//     assertEquals(
+//       9,
+//       solution.findMedianSortedArrays(n1, n2)
+//     );
+//   }
 
-  @Test
-  void Example5() {
-    var solution = new Solution();
-    var n1 = new int[]{2, 3, 4, 5};
-    var n2 = new int[]{};
+//   @Test
+//   void Example5() {
+//     var solution = new Solution();
+//     var n1 = new int[]{2, 3, 4, 5};
+//     var n2 = new int[]{};
 
-    assertEquals(
-      3.5,
-      solution.findMedianSortedArrays(n1, n2)
-    );
-  }
-}
+//     assertEquals(
+//       3.5,
+//       solution.findMedianSortedArrays(n1, n2)
+//     );
+//   }
+// }
