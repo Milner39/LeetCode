ee8327b190966557007a7a7754e4b99388c84b3b
Milner39 <91906877+Milner39@users.noreply.github.com>
2025-09-22 18:41:35 +0100

    fix: dependencies correctly versioned and passed to subprojects

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,53 +1,3 @@
-// plugins {
-//   // Compiling Java
-//   id("java-library")
-
-//   // Adds the `run` task
-//   id("application")
-// }
-
-
-// java {
-//   toolchain {
-//     // Use specific Java version
-//     languageVersion = JavaLanguageVersion.of(21)
-//   }
-// }
-
-
-// // Tell Gradle to use Maven Central for dependencies
-// repositories {
-//   mavenCentral()
-// }
-
-// dependencies {
-//   // JUnit for unit tests
-//   testImplementation(libs.junit)
-//   testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-// }
-
-
-// tasks.named<Test>("test") {
-//   // Use JUnit Platform for unit tests.
-//   useJUnitPlatform()
-// }
-
-
-// configure<SourceSetContainer> {
-//   named("main") {
-//     // Gradle will look for main code in:
-//     // `./src` instead of `./src/main/java`
-//     java.srcDirs("src")
-//   }
-//   named("test") {
-//     // Gradle will look for test code in:
-//     // `./test` instead of `./src/test/java`
-//     java.srcDirs("test")
-//   }
-// }
-
-
-
 subprojects {
   // === Plugins ===
 
@@ -75,6 +25,8 @@ subprojects {
 
 
   // === Dependencies ===
+
+  var libs = rootProject.libs
 
   // Tell Gradle to use Maven Central for dependencies
   repositories {
