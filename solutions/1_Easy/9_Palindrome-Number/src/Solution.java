91721ad879fc971aec5d50996ee609f48cc1e25d
Milner39 <91906877+Milner39@users.noreply.github.com>
2025-09-22 13:54:53 +0100

    feat: problem 9 v2

--- solutions/1_Easy/9_Palindrome-Number/src/Solution.java
+++ solutions/1_Easy/9_Palindrome-Number/src/Solution.java
@@ -4,60 +4,23 @@ class Solution {
     // Negative numbers cannot be palindromes
     if (x < 0) return false;
 
-    // Single digit numbers are always palindromes
+    // // Single digit numbers are always palindromes
     if (x < 10) return true;
 
 
     // Get the number of digits
-    int digits = 0;
-    int y = x;
-    while (y > 0) {
-      digits++;
-      y = y / 10;
+    int d = 2;
+    while (x >= Math.pow(10, d)) {
+      d++;
     }
 
-    // Calculate the number of comparisons that need to be made
-    int iterations = digits / 2;
+    // Iteratively check and remove the left and right most digits
+    while (x > 0) {
+      if ((x / (int) Math.pow(10, d-1)) != (x % 10)) return false;
 
-
-    // Get the left/right most digits are compare them each iteration
-    int count = 0;
-    while (count < iterations) {
-      int left = x / (int) Math.pow(10, (digits - 1 - count));
-      int right = x % (int) Math.pow(10, (count + 1)) / (int) Math.pow(10, count);
-
-      // Not palindrome
-      if (left != right) return false;
-
-      // Subtract the current digit to make the maths easier next iteration
-      x -= left * Math.pow(10, (digits - 1 - count));
-
-      // Next iteration
-      count++;
+      x = (x % (int) Math.pow(10, d-1)) / 10;
+      d -= 2;
     }
-
-
-    /*
-      1d: always palindromes
-
-      2d: (x / 10) must equal (x % 10)
-
-      3d: (x / 100) must equal (x % 10)
-
-      4d: (x / 1000) must equal (x % 10)
-          x - 1000 * left
-          (x / 100) must equal (x % 100 / 10)
-      
-      5d: (x / 10000) must equal (x % 10)
-          x - 10000 * left
-          (x / 1000) must equal (x % 100 / 10)
-
-      6d: (x / 100000) must equal (x % 10)
-          x - 100000 * left
-          (x / 10000) must equal (x % 100 / 10)
-          x - 10000 * left
-          (x / 1000) must equal (x % 1000 / 100)
-    */
 
     return true;
   }
