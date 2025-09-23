dee43f91185a92db9b6c180788fb71e5b636d5a9
Milner39 <91906877+Milner39@users.noreply.github.com>
2025-09-23 12:39:51 +0100

    style

--- solutions/1_Easy/9_Palindrome-Number/test/SolutionTest.java
+++ solutions/1_Easy/9_Palindrome-Number/test/SolutionTest.java
@@ -2,31 +2,31 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SolutionTest {
-	
-	@Test
-	void Example1() {
-		var solution = new Solution();
-		assertEquals(
-			true,
-			solution.isPalindrome(121)
-		);
-	}
 
-	@Test
-	void Example2() {
-		var solution = new Solution();
-		assertEquals(
-			false,
-			solution.isPalindrome(-121)
-		);
-	}
+  @Test
+  void Example1() {
+    var solution = new Solution();
+    assertEquals(
+      true,
+      solution.isPalindrome(121)
+    );
+  }
 
-	@Test
-	void Example3() {
-		var solution = new Solution();
-		assertEquals(
-			false,
-			solution.isPalindrome(10)
-		);
-	}
+  @Test
+  void Example2() {
+    var solution = new Solution();
+    assertEquals(
+      false,
+      solution.isPalindrome(-121)
+    );
+  }
+
+  @Test
+  void Example3() {
+    var solution = new Solution();
+    assertEquals(
+      false,
+      solution.isPalindrome(10)
+    );
+  }
 }
