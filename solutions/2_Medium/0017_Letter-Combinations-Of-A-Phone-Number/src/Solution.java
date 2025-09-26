8718cd51cba1a1cd7e546d12959b3d9435f94dec
Milner39 <91906877+Milner39@users.noreply.github.com>
2025-09-27 00:19:30 +0100

    refactor: do it the proper way

--- solutions/2_Medium/0017_Letter-Combinations-Of-A-Phone-Number/src/Solution.java
+++ solutions/2_Medium/0017_Letter-Combinations-Of-A-Phone-Number/src/Solution.java
@@ -3,72 +3,50 @@ import java.util.List;
 import java.util.Map;
 
 class Solution {
+
+  // Map numbers to corresponding chars
+  private static final Map<Character, char[]> digitToChars = Map.<Character, char[]>ofEntries(
+    Map.entry('2', new char[]{'a','b','c'}),
+    Map.entry('3', new char[]{'d','e','f'}),
+    Map.entry('4', new char[]{'g','h','i'}),
+    Map.entry('5', new char[]{'j','k','l'}),
+    Map.entry('6', new char[]{'m','n','o'}),
+    Map.entry('7', new char[]{'p','q','r','s'}),
+    Map.entry('8', new char[]{'t','u','v'}),
+    Map.entry('9', new char[]{'w','x','y','z'})
+  );
+
+
+  private void backtrack(List<String> res, StringBuilder sb, String d, int i) {
+    // If we have added a char for each index
+    if (i == d.length()) {
+      // Add string to results and return
+      res.add(sb.toString());
+      return;
+    }
+
+    for (char c : digitToChars.get(d.charAt(i))) {
+      sb.append(c);
+      backtrack(res, sb, d, i+1);
+      sb.deleteCharAt(sb.length()-1);
+    }
+  }
+
+
   public List<String> letterCombinations(String digits) {
 
-    int len = digits.length();
-
-    if (len == 0) return List.of();
-
-
-    // Map numbers to corresponding chars
-    Map<Character, char[]> digitToChars = Map.<Character, char[]>ofEntries(
-      Map.entry('2', new char[]{'a','b','c'}),
-      Map.entry('3', new char[]{'d','e','f'}),
-      Map.entry('4', new char[]{'g','h','i'}),
-      Map.entry('5', new char[]{'j','k','l'}),
-      Map.entry('6', new char[]{'m','n','o'}),
-      Map.entry('7', new char[]{'p','q','r','s'}),
-      Map.entry('8', new char[]{'t','u','v'}),
-      Map.entry('9', new char[]{'w','x','y','z'})
-    );
-
-
-    // Store number of combinations for faster `ArrayList` access later
+    // Store number of combinations for faster `ArrayList` appending later
     int numCombos = 1;
-
-    // Store the characters that correspond to each digit in `digits`
-    char[][] charComboOrder = new char[len][];
-
-    // Iterate over `digits` to populate our variables
-    char[] digitArray = digits.toCharArray();
-    for (int i = 0; i < len; i++) {
-      char[] chars = digitToChars.get(digitArray[i]);
-      numCombos *= chars.length;
-      charComboOrder[i] = chars;
+    for (char digit : digits.toCharArray()) {
+      numCombos *= digitToChars.get(digit).length;
     }
 
-
-    // List of `StringBuilder`s for faster concatenation
-    List<StringBuilder> combinations = new ArrayList<>(numCombos);
-    for (int i = 0; i < numCombos; i++) {
-      combinations.add(new StringBuilder());
-    }
-
-    // Fill out our combinations
-    int groups = 1;
-    for (int i = 0; i < len; i++) {
-      // For each digit, get corresponding chars
-      char[] chars = charComboOrder[i];
-      int options = chars.length;  // Get the number of chars for that digit
-      groups *= options;  // Get the number of possible combinations this iteration
-
-      for (int j = 0; j < numCombos; j++) {
-        // If only a third of the total combinations are possible this iteration, 
-        // Then the current char will show up 3 times before moving onto the next 
-        // one.
-        int charIndex = (j*groups/numCombos)%options;
-        combinations.get(j).append(chars[charIndex]);
-      }
-    }
-
-    // Create a new `ArrayList` and convert all the combinations into strings
+    // Create result list with know size
     List<String> result = new ArrayList<>(numCombos);
-    for (int i = 0; i < numCombos; i++) {
-      result.add(combinations.get(i).toString());
-    }
+    if (digits.length() == 0) return result;
+
+    backtrack(result, new StringBuilder(digits.length()), digits, 0);
 
     return result;
   }
 }
-
-// This is probably not the right way, but it's my way
--- solutions/2_Medium/0017_Letter-Combinations-Of-A-Phone-Number/test/SolutionTest.java
+++ solutions/2_Medium/0017_Letter-Combinations-Of-A-Phone-Number/test/SolutionTest.java
@@ -1,45 +1,46 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+// import org.junit.jupiter.api.Test;
+// import static org.junit.jupiter.api.Assertions.*;
 
-public class SolutionTest {
+// public class SolutionTest {
 
-  @Test
-  void Example1() {
-    var solution = new Solution();
-    assertArrayEquals(
-      new String[]{"ad","ae","af","bd","be","bf","cd","ce","cf"},
-      solution.letterCombinations("23").toArray()
-    );
-  }
+//   // TODO: Proper tests for this problem
+//   @Test
+//   void Example1() {
+//     var solution = new Solution();
+//     assertArrayEquals(
+//       new String[]{"ad","ae","af","bd","be","bf","cd","ce","cf"},
+//       solution.letterCombinations("23").toArray()
+//     );
+//   }
 
-  @Test
-  void Example2() {
-    var solution = new Solution();
-    assertArrayEquals(
-      new String[]{},
-      solution.letterCombinations("").toArray()
-    );
-  }
+//   @Test
+//   void Example2() {
+//     var solution = new Solution();
+//     assertArrayEquals(
+//       new String[]{},
+//       solution.letterCombinations("").toArray()
+//     );
+//   }
 
-  @Test
-  void Example3() {
-    var solution = new Solution();
-    assertArrayEquals(
-      new String[]{"a","b","c"},
-      solution.letterCombinations("2").toArray()
-    );
-  }
+//   @Test
+//   void Example3() {
+//     var solution = new Solution();
+//     assertArrayEquals(
+//       new String[]{"a","b","c"},
+//       solution.letterCombinations("2").toArray()
+//     );
+//   }
 
-  @Test
-  void Example4() {
-    var solution = new Solution();
-    assertArrayEquals(
-      new String[]{
-        "apg","aph","api","aqg","aqh","aqi","arg","arh","ari","asg","ash","asi",
-        "bpg","bph","bpi","bqg","bqh","bqi","brg","brh","bri","bsg","bsh","bsi",
-        "cpg","cph","cpi","cqg","cqh","cqi","crg","crh","cri","csg","csh","csi"
-      },
-      solution.letterCombinations("274").toArray()
-    );
-  }
-}
+//   @Test
+//   void Example4() {
+//     var solution = new Solution();
+//     assertArrayEquals(
+//       new String[]{
+//         "apg","aph","api","aqg","aqh","aqi","arg","arh","ari","asg","ash","asi",
+//         "bpg","bph","bpi","bqg","bqh","bqi","brg","brh","bri","bsg","bsh","bsi",
+//         "cpg","cph","cpi","cqg","cqh","cqi","crg","crh","cri","csg","csh","csi"
+//       },
+//       solution.letterCombinations("274").toArray()
+//     );
+//   }
+// }
